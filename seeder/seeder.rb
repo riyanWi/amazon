7800a54809accc701fcbc1e69ee086bb70abbf41
Riyan Wibowo <tunggalrahayu1@gmail.com>
2022-03-02 23:40:39 +0700

    fix seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,7 +2,7 @@ pages << {
     page_type: "listings",
     method: "GET",
     headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-    url: "https://www.amazon.com/s?rh=n%3A6459737011&fs=true",
+    url: "https://www.amazon.com/s?rh=n%3A6459737011&fs=true&ref=lp_6459737011_sar",
     vars: {
       category: "LED & LCD TVs"
     }
@@ -11,7 +11,7 @@ pages << {
     page_type: "listings",
     method: "GET",
     headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-    url: "https://www.amazon.com/s?rh=n%3A6463520011&fs=true",
+    url: "https://www.amazon.com/s?rh=n%3A6463520011&fs=true&ref=lp_6463520011_sar",
     vars: {
       category: "OLED TVs"
     }
